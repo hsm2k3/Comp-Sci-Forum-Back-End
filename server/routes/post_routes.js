1d5925c6e56dcd20abc6f2418d5dc9cc2b82f385
Yussef <saidiyussef18@gmail.com>
2019-04-26 15:52:35 -0400

    Complete POST API

--- server/routes/post_routes.js
+++ server/routes/post_routes.js
@@ -2,11 +2,11 @@ const PostController = require('../controllers/index_controllers').posts;
 
 
 const section_routes = (app) => {
-    app.post('/api/posts', PostController.create); //Create new section
-    app.get('/api/posts', PostController.list); //Get all sections
-    app.get('api/posts/:postId', PostController.list); //Get one specific section
-    app.put('api/postss/:postId', PostController.update); //Update a section
-    app.delete('api/posts/:postId', PostController.destroy); //Delete a section
+    app.post('/api/posts', PostController.create); //Create new post
+    app.get('/api/posts', PostController.list); //Get all posts
+    app.get('api/posts/:postId', PostController.list); //Get one specific post
+    app.put('api/postss/:postId', PostController.update); //Update a post
+    app.delete('api/posts/:postId', PostController.destroy); //Delete a post
 };
 
 
