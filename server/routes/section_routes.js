63433e443c13265f2f6a6f5a3ca294be5197a89c
mbajor14 <michael.bajor@protonmail.com>
2019-04-29 22:04:26 -0400

    Added route for getting section data searched by code

--- server/routes/section_routes.js
+++ server/routes/section_routes.js
@@ -4,9 +4,10 @@ const SectionController = require('../controllers/index_controllers').sections;
 const section_routes = (app) => {
     app.post('/api/sections', SectionController.create); //Create new section
     app.get('/api/sections', SectionController.list); //Get all sections
-    app.get('api/sections/:sectionId', SectionController.list); //Get one specific section
-    app.put('api/sections/:sectionId', SectionController.update); //Update a section
-    app.delete('api/sections/:sectionId', SectionController.destroy); //Delete a section
+    app.get('/api/sections/:sectionId', SectionController.list); //Get one specific section by id
+    app.get('/api/sections/code/:sectionCode', SectionController.searchByCode); //Get one specific section by code
+    app.put('/api/sections/:sectionId', SectionController.update); //Update a section
+    app.delete('/api/sections/:sectionId', SectionController.destroy); //Delete a section
 };
 
 
